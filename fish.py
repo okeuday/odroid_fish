903b06bb24e361ccdb134ebccd19035eaf6b4dcf
Michael Truog <mjtruog@protonmail.com>
2018-12-18 19:15:57 -0800

    Update for CloudI 1.7.5.

--- fish.py
+++ fish.py
@@ -26,7 +26,7 @@
 #
 
 import sys
-sys.path.append('/usr/local/lib/cloudi-1.7.4/api/python/')
+sys.path.append('/usr/local/lib/cloudi-1.7.5/api/python/')
 
 import threading, types, traceback
 from cloudi import API, terminate_exception
