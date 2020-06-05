a059542b576c4337ceb987304239c4b3b5aa0c3a
Michael Truog <mjtruog@protonmail.com>
2020-06-04 20:20:28 -0700

    Update for CloudI 2.0.0.

--- fish.py
+++ fish.py
@@ -28,7 +28,7 @@
 # pylint: disable=wrong-import-position
 # pylint: disable=wrong-import-order
 import sys
-sys.path.append('/usr/local/lib/cloudi-1.8.0/api/python/')
+sys.path.append('/usr/local/lib/cloudi-2.0.0/api/python/')
 from cloudi import API, TerminateException
 import threading
 import traceback
